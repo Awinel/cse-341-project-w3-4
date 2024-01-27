5e039a4e35755acafe56bf4d4333fb689bc53596
Awinel <benjamin.huerta.awi@gmail.com>
2024-01-27 09:29:50 -0300

    swagger update

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+    info: {
+        title: "Clinical API",
+        description: "Clinical API to be used to organize data and keep track of all the patiente information"
+    },
+    host:"localhost/3000",
+    schemes: ["http", "https"]
+};
+
+const outputfile = "./swagger.json";
+const endpointfiles = ["./routes/index.js"];
+
+swaggerAutogen(outputfile, endpointfiles, doc);
